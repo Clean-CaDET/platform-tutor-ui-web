577fb09b55c1ecd320af7704868b67a1ed4e1ee6
Nikola Pantelic <nikola.pantelic@uns.ac.rs>
2021-04-12 17:06:26 +0200

    feature: Markdown tag insertion

--- src/app/lecture/knowledge-node/notes/markdown-editor/markdown-editor.component.ts
+++ src/app/lecture/knowledge-node/notes/markdown-editor/markdown-editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'cc-markdown-editor',
@@ -7,13 +7,70 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class MarkdownEditorComponent implements OnInit {
 
-  text: string;
+  text = '';
   livePreview = true;
+  selection: any;
+  @ViewChild('textAreaElement') textArea: HTMLTextAreaElement;
   @Input() mode = 'edit';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  insertElement(type: string): void {
+    let tagBegin = '';
+    let tagEnd = '';
+    let tagText = '';
+    switch (type) {
+      case 'bold':
+        tagBegin = tagEnd = '**';
+        tagText = 'Bold Text';
+        break;
+      case 'italic':
+        tagBegin = tagEnd = '_';
+        tagText = 'Italic Text';
+        break;
+      case 'bulleted':
+        tagBegin = '- ';
+        tagText = 'List Item';
+        break;
+      case 'numbered':
+        tagBegin = '1. ';
+        tagText = 'List Item';
+        break;
+      case 'code':
+        tagBegin = tagEnd = '```';
+        tagText = 'Code';
+        break;
+    }
+    if (this.selection) {
+      if (this.selection.selectionStart !== this.selection.selectionEnd) {
+        tagText = this.text.slice(this.selection.selectionStart, this.selection.selectionEnd);
+      }
+    }
+    const text = tagBegin + tagText + tagEnd;
+
+    if (this.selection) {
+      console.log(this.selection);
+      this.text = this.text.slice(0, this.selection.selectionStart) + text + this.text.slice(this.selection.selectionEnd);
+    } else {
+      console.log(this.textArea.selectionStart);
+      this.text += text;
+    }
+  }
+
+  onSelect(event): void {
+    console.log(event.target.selectionStart + ' ' + event.target.selectionEnd);
+    this.selection = event.target;
+  }
+
+  onChange(event): void {
+    console.log(event);
+  }
+
+  onClick(event): void {
+    this.selection = event.target;
   }
 
 }
