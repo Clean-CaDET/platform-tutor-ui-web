97221f0328da7ec51697d982a727272eb6316594
Nikola Pantelic <nikola.pantelic@uns.ac.rs>
2021-04-13 17:51:06 +0200

    feat: Added trainee ID to arrange task and question submissions

--- src/app/lecture/knowledge-node/learning-objects/arrange-task/service/arrange-task.service.ts
+++ src/app/lecture/knowledge-node/learning-objects/arrange-task/service/arrange-task.service.ts
@@ -3,17 +3,21 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Container } from '../model/container.model';
 import { environment } from '../../../../../../environments/environment';
+import { TraineeService } from '../../../../../trainee/service/trainee.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArrangeTaskService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private traineeService: TraineeService) { }
 
-  submitTask(nodeId: number, arrangeTaskId: number, state: Container[]): Observable<any> {
+  submitTask(nodeId: number, arrangeTaskId: number, containers: Container[]): Observable<any> {
     return this.http.post(
       environment.apiHost + 'nodes/' + nodeId + '/content/arrange-task/' + arrangeTaskId,
-      state);
+      {
+        traineeId: this.traineeService.trainee$.value.id,
+        containers
+      });
   }
 }
--- src/app/trainee/model/trainee.model.ts
+++ src/app/trainee/model/trainee.model.ts
@@ -1,9 +1,19 @@
 export class Trainee {
+  id: number;
   studentIndex: string;
+  visualScore: number;
+  auralScore: number;
+  readWriteScore: number;
+  kinaestheticScore: number;
 
   constructor(obj?: any) {
     if (obj) {
+      this.id = obj.id;
       this.studentIndex = obj.studentIndex;
+      this.visualScore = obj.visualScore;
+      this.auralScore = obj.auralScore;
+      this.readWriteScore = obj.readWriteScore;
+      this.kinaestheticScore = obj.kinaestheticScore;
     }
   }
 }
