7be667da2acade6ca63c86beb083e11af7e180a6
lukaDoric <doricluka12@gmail.com>
2022-07-11 21:51:05 +0200

    Instructor is able to fetch and export all events to CSV.

--- src/app/modules/learner-analytics/events-table/events-table.component.ts
+++ src/app/modules/learner-analytics/events-table/events-table.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { LearnerAnalyticsService } from '../learner-analytics.service';
-import { LearningEvent } from './learning-event';
+import {Component, OnInit} from '@angular/core';
+import {LearnerAnalyticsService} from '../learner-analytics.service';
+import {LearningEvent} from './learning-event';
 import {ngxCsv} from 'ngx-csv';
 
 @Component({
@@ -10,12 +10,25 @@ import {ngxCsv} from 'ngx-csv';
 })
 export class EventsTableComponent implements OnInit {
   events: LearningEvent[];
+  allEvents: LearningEvent[];
   count: number;
   page = 1;
   pageSize = 50;
   displayedColumns: string[] = ['timeStamp', 'type', 'kcId', 'learnerId', 'specificData'];
+  exportOptions = {
+    fieldSeparator: ',',
+    quoteStrings: '"',
+    decimalseparator: '.',
+    showLabels: true,
+    showTitle: true,
+    title: 'Events',
+    useBom: true,
+    noDownload: false,
+    headers: ['Timestamp', 'Type', 'Knowledge Component Id', 'Learner Id', 'Event-specific data']
+  };
 
-  constructor(private analyticsService: LearnerAnalyticsService) { }
+  constructor(private analyticsService: LearnerAnalyticsService) {
+  }
 
   ngOnInit(): void {
     this.getEvents();
@@ -35,24 +48,24 @@ export class EventsTableComponent implements OnInit {
   }
 
   exportToCSV(): void {
-    const options = {
-      fieldSeparator: ',',
-      quoteStrings: '"',
-      decimalseparator: '.',
-      showLabels: true,
-      showTitle: true,
-      title: 'Events',
-      useBom: true,
-      noDownload: false,
-      headers: ['Timestamp', 'Type', 'Knowledge Component Id', 'Learner Id', 'Event-specific data']
-    };
-
     const data = JSON.parse(JSON.stringify(this.events));
     for (const event of data) {
       if (event.specificData) {
         event.specificData = JSON.stringify(event.specificData);
       }
     }
-    new ngxCsv(data, 'Events', options);
+    new ngxCsv(data, 'Events', this.exportOptions);
+  }
+
+  exportAllToCSV(): void {
+    this.analyticsService.getAllEvents().subscribe(data => {
+      this.allEvents = data.events.sort((a, b) => a.timeStamp.getTime() - b.timeStamp.getTime());
+      for (const event of this.allEvents) {
+        if (event.specificData) {
+          event.specificData = JSON.stringify(event.specificData);
+        }
+      }
+      new ngxCsv(this.allEvents, 'Events', this.exportOptions);
+    });
   }
 }
