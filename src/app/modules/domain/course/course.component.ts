ddb3ae531847e16acb7866620ea9d041bbbe44c3
lukaDoric <doricluka12@gmail.com>
2022-10-04 09:24:08 +0200

    feat: learner is able to choose courses.

--- src/app/modules/domain/course/course.component.ts
+++ src/app/modules/domain/course/course.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
 import {Course} from './course.model';
 import {CourseService} from './course.service';
+import {AuthenticationService} from '../../../infrastructure/auth/auth.service';
+import {User} from '../../../infrastructure/auth/user.model';
 
 @Component({
   selector: 'cc-course-component',
@@ -11,14 +13,19 @@ import {CourseService} from './course.service';
 export class CourseComponent implements OnInit {
 
   course: Course;
+  user: User;
 
   constructor(private route: ActivatedRoute,
-              private courseService: CourseService) {
+              private courseService: CourseService,
+              private authService: AuthenticationService) {
   }
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
       this.courseService.getCourse(+params.courseId).subscribe(course => this.course = course);
+    });
+    this.authService.user$.subscribe(user => {
+      this.user = user;
     });
   }
 }
