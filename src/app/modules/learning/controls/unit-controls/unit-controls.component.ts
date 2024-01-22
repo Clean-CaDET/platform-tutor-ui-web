7c4a71ba694a05db9d2315a9524db01971f6225f
Nikola Luburic <luburic91@gmail.com>
2024-01-22 13:34:20 +0100

    feat: Migrates KC navigation to navbar.

--- src/app/modules/learning/controls/unit-controls/unit-controls.component.ts
+++ src/app/modules/learning/controls/unit-controls/unit-controls.component.ts
@@ -1,22 +1,29 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'cc-unit-controls',
   templateUrl: './unit-controls.component.html',
   styleUrls: ['./unit-controls.component.scss']
 })
-export class UnitControlsComponent implements OnInit {
+export class UnitControlsComponent {
   @Input() unitId: number;
   @Input() courseId: number;
   @Input() notesExpanded: boolean;
   @Output() expandNotes = new EventEmitter();
-
+  
+  @Input() kcConfig: any;
+  @Output() kcViewSwapped = new EventEmitter();
+  
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
   openNotes() {
     this.expandNotes.emit(!this.notesExpanded);
+  }
+
+  swapKcView(isInstruction: boolean): void {
+    if(isInstruction && this.kcConfig.instructionActive) return;
+    if(!isInstruction && !this.kcConfig.instructionActive) return;
+    if(isInstruction) this.kcViewSwapped.emit("IE");
+    else this.kcViewSwapped.emit("AE");
   }
 }
