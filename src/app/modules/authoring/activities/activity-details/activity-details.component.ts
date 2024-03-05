cb95bc6075551d56262f970138197080501c62e7
Nikola Luburic <luburic91@gmail.com>
2024-03-05 09:23:34 +0100

    refactor: Removes unneeded logic from activity form builder.

--- src/app/modules/authoring/activities/activity-details/activity-details.component.ts
+++ src/app/modules/authoring/activities/activity-details/activity-details.component.ts
@@ -13,15 +13,11 @@ export class ActivityDetailsComponent implements OnChanges {
 
   activityForm: FormGroup;
   editMode: boolean = false;
-  editExampleModes: boolean[];
-  editStandardModes: boolean[];
 
   constructor(private builder: FormBuilder, private cdr: ChangeDetectorRef) { }
 
   ngOnChanges(): void {
     this.createForm();
-    this.editExampleModes = new Array(this.activity.examples.length).fill(false);
-    this.editStandardModes = new Array(this.activity.standards.length).fill(false);
     if (this.activity) {
       this.setInitialValues(this.activity);
     }
@@ -52,7 +48,16 @@ export class ActivityDetailsComponent implements OnChanges {
     this.activityForm.setControl('examples', examplesArray);
     const standarsArray = this.setStandards(activity);
     this.activityForm.setControl('standards', standarsArray);
+    
     this.cdr.detectChanges();
+  }
+
+  get examples(): any {
+    return this.activityForm.get('examples') as FormArray;
+  }
+  
+  get standards(): any {
+    return this.activityForm.get('standards') as FormArray;
   }
 
   setExamples(activity: any) {
@@ -78,98 +83,30 @@ export class ActivityDetailsComponent implements OnChanges {
   }
 
   addExample(): void {
-    const examplesArray = this.activityForm.get('examples') as FormArray;
-    examplesArray.push(this.builder.group({
+    this.examples.push(this.builder.group({
       code: new FormControl('', Validators.required),
       description: new FormControl('', Validators.required)
     }));
-    this.editExampleModes.push(true);
   }
 
   deleteExample(index: number): void {
-    const examplesArray = this.activityForm.get('examples') as FormArray;
-    examplesArray.removeAt(index);
-    this.editExampleModes.splice(index, 1);
-    this.activitySaved.emit(this.activityForm.value);
-  }
-
-  get examples(): any {
-    return this.activityForm.get('examples') as FormArray;
-  }
-
-  getExampleControl(index: number, controlName: string): FormControl {
-    const examplesArray = this.activityForm.get('examples') as FormArray;
-    return (examplesArray.at(index) as FormGroup).get(controlName) as FormControl;
-  }
-
-  updateExampleDescription(newDescription: string, index: number): void {
-    this.getExampleControl(index, 'description').setValue(newDescription);
-  }
-
-  updateExample(i: number) {
-    if (this.activityForm.valid) {
-      this.editExampleModes[i] = false;
-      this.activitySaved.emit(this.activityForm.value);
-    }
-  }
-
-  discardExampleChanges(index: number) {
-    this.editExampleModes[index] = false;
-    if (this.activity.examples[index]) {
-      this.getExampleControl(index, 'code').setValue(this.activity.examples[index].code);
-      this.getExampleControl(index, 'description').setValue(this.activity.examples[index].description);
-    } else {
-      this.deleteExample(index);
-    }
+    this.examples.removeAt(index);
   }
 
   addStandard() {
-    const standardsArray = this.activityForm.get('standards') as FormArray;
-    standardsArray.push(this.builder.group({
+    this.standards.push(this.builder.group({
       name: new FormControl('', Validators.required),
       description: new FormControl('', Validators.required)
     }));
-    this.editStandardModes.push(true);
   }
 
   deleteStandard(index: number): void {
-    const standardsArray = this.activityForm.get('standards') as FormArray;
-    standardsArray.removeAt(index);
-    this.editStandardModes.splice(index, 1);
-    this.activitySaved.emit(this.activityForm.value);
-  }
-
-  get standards(): any {
-    return this.activityForm.get('standards') as FormArray;
-  }
-
-  getStandardControl(index: number, controlName: string): FormControl {
-    const standardsArray = this.activityForm.get('standards') as FormArray;
-    return (standardsArray.at(index) as FormGroup).get(controlName) as FormControl;
-  }
-
-  updateStandard(i: number) {
-    if (this.activityForm.valid) {
-      this.editStandardModes[i] = false;
-      this.activitySaved.emit(this.activityForm.value);
-    }
-  }
-
-  discardStandardChanges(index: number) {
-    this.editStandardModes[index] = false;
-    if (this.activity.examples[index]) {
-      this.getStandardControl(index, 'name').setValue(this.activity.standards[index].name);
-      this.getStandardControl(index, 'description').setValue(this.activity.standards[index].description);
-    } else {
-      this.deleteStandard(index);
-    }
+    this.standards.removeAt(index);
   }
 
   submitForm() {
     if (this.activityForm.valid) {
       this.editMode = false;
-      this.editExampleModes = new Array(this.activity.examples.length).fill(false);
-      this.editStandardModes = new Array(this.activity.standards.length).fill(false);
       this.activitySaved.emit(this.activityForm.value);
     }
   }
@@ -179,7 +116,5 @@ export class ActivityDetailsComponent implements OnChanges {
     if (this.activity.id != undefined) {
       this.editMode = false;
     }
-    this.editExampleModes = new Array(this.activity.examples.length).fill(false);
-    this.editStandardModes = new Array(this.activity.standards.length).fill(false);
   }
 }
