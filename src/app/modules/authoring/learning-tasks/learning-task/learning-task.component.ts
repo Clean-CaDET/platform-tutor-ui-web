9f61c4e070ae178e96794003d304a20eeee348e0
Nikola Luburic <luburic91@gmail.com>
2024-03-05 07:43:46 +0100

    partial: Improves LT activity details view.

--- src/app/modules/authoring/learning-tasks/learning-task/learning-task.component.ts
+++ src/app/modules/authoring/learning-tasks/learning-task/learning-task.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LearningTask } from '../model/learning-task';
 
 @Component({
   selector: 'cc-learning-task',
@@ -6,8 +7,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./learning-task.component.scss']
 })
 export class LearningTaskComponent {
+  task: LearningTask;
+  taskDescription: string;
 
-  constructor() { }
+  editMode: false;
+  activities: any[];
 
+  constructor() {
+    this.task = {
+      id: 1,
+      unitId: 1,
+      isTemplate: true,
+      name: "Razvoj složene funkcionalnosti",
+      steps: [
+        {
+          id: 1,
+          order: 1,
+          activityId: 1,
+          activityName: "Dizajniranje strukture i ponašanja agregata"
+        },
+        {
+          id: 2,
+          order: 2,
+          activityId: 2,
+          activityName: "Implementacija agregata"
+        },
+        {
+          id: 3,
+          order: 3,
+          activityId: 3,
+          activityName: "Testiranje agregata"
+        }
+      ]
+    };
 
+    this.activities = [
+      {
+        id: 1,
+        courseId: 1,
+        code: "AgregatD",
+        name: "Dizajniranje strukture i ponašanja agregata"
+      },
+      {
+        id: 2,
+        courseId: 1,
+        code: "AgregatI",
+        name: "Implementacija agregata"
+      },
+      {
+        id: 3,
+        courseId: 1,
+        code: "AgregatT",
+        name: "Testiranje agregata"
+      }
+    ]
+  }
+
+  updateDescription(description: string) {
+    this.taskDescription = description;
+  }
 }
--- src/app/modules/authoring/learning-tasks/model/submission-format.ts
+++ src/app/modules/authoring/learning-tasks/model/submission-format.ts
@@ -1,4 +1,3 @@
-
 export interface SubmissionFormat {
     submissionGuidelines: string;
     answerValidation: string;
