bbc07d933fa6dba9723e278aa98766c752bddebd
Nikola Luburic <luburic91@gmail.com>
2024-04-26 08:06:01 +0200

    feat: Adds previous and next KC navigation buttons to KC authoring.

--- src/app/modules/authoring/knowledge-component/knowledge-component-authoring.component.ts
+++ src/app/modules/authoring/knowledge-component/knowledge-component-authoring.component.ts
@@ -10,20 +10,35 @@ import { KnowledgeComponentService } from './knowledge-component-authoring.servi
 })
 export class KnowledgeComponentAuthoringComponent implements OnInit, OnDestroy {
   courseId: number;
+  allKcs: KnowledgeComponent[];
+  
   kc: KnowledgeComponent;
+  nextKc: KnowledgeComponent;
+  prevKc: KnowledgeComponent;
 
   routeSubscription: any;
 
   constructor(private kcService: KnowledgeComponentService, private route: ActivatedRoute) { }
 
-  // Should be reworked to integrate this small component into instructional items.
   ngOnInit(): void {
     this.routeSubscription = this.route.params.subscribe((params: Params) => {
-      this.courseId = +params.courseId;
-      this.kcService.get(+params.kcId).subscribe(kc => {
-        this.kc = kc;
-      });
+      if(!this.allKcs?.length) {
+        this.courseId = +params.courseId;
+        this.kcService.getByUnit(+params.unitId).subscribe(kcs => {
+          this.allKcs = kcs.sort((a, b) => a.order - b.order);
+          this.setKcs(+params.kcId);
+        });
+      } else {
+        this.setKcs(+params.kcId);
+      }
     });
+  }
+
+  private setKcs(activeKcId: number) {
+    this.kc = this.allKcs.find(kc => kc.id == activeKcId);
+    const index = this.allKcs.findIndex(kc => kc.id == activeKcId);
+    this.prevKc = index != 0 ? this.allKcs[index-1] : null;
+    this.nextKc = index != this.allKcs.length - 1 ? this.allKcs[index+1] : null;
   }
 
   ngOnDestroy(): void {
