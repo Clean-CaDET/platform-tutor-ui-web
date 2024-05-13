563b55b41344105890634f58a1e738152626e4fc
Nikola Luburic <luburic91@gmail.com>
2024-05-13 10:35:29 +0200

    fix: Slight UI consistency improvement for management controls.

--- src/app/modules/layout/home/dashboard/dashboard.component.ts
+++ src/app/modules/layout/home/dashboard/dashboard.component.ts
@@ -1,39 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from 'src/app/infrastructure/auth/auth.service';
+import { Component, Input } from '@angular/core';
 import { User } from 'src/app/infrastructure/auth/user.model';
 import { Course } from 'src/app/modules/learning/model/course.model';
-import { LayoutService } from '../../layout.service';
 
 @Component({
   selector: 'cc-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit {
-  user: User = null;
-  courses: Course[] = [];
+export class DashboardComponent {
+  @Input() user: User;
+  @Input() courses: Course[];
 
-  constructor(
-    private authService: AuthenticationService,
-    private layoutService: LayoutService
-  ) {}
-
-  ngOnInit(): void {
-    this.authService.user$.subscribe((user) => {
-      this.user = user;
-      if (this.user === null) {
-        this.courses = [];
-        return;
-      }
-      if (this.user.role.includes('learner')) {
-        this.layoutService.getLearnerCourses().subscribe((coursesPage) => {
-          this.courses = coursesPage.results;
-        });
-      } else if (this.user.role === 'instructor') {
-        this.layoutService.getInstructorCourses().subscribe((courses) => {
-          this.courses = courses;
-        });
-      }
-    });
-  }
+  constructor() {}
 }
--- src/app/modules/layout/home/home.component.ts
+++ src/app/modules/layout/home/home.component.ts
@@ -1,10 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/infrastructure/auth/auth.service';
+import { User } from 'src/app/infrastructure/auth/user.model';
+import { Course } from 'src/app/modules/learning/model/course.model';
+import { LayoutService } from '../layout.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'cc-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
-  constructor() {}
+export class HomeComponent implements OnInit, OnDestroy {
+  userSubscription: Subscription;
+  user: User = null;
+  courses: Course[] = [];
+
+  constructor(private authService: AuthenticationService, private layoutService: LayoutService) {}
+
+  ngOnInit(): void {
+    this.userSubscription = this.authService.user$.subscribe((user) => {
+      this.user = user;
+      if (!this.user) {
+        this.courses = [];
+        return;
+      }
+      if (this.user.role.includes('learner')) {
+        this.layoutService.getLearnerCourses().subscribe((coursesPage) => {
+          this.courses = coursesPage.results;
+        });
+      } else if (this.user.role === 'instructor') {
+        this.layoutService.getInstructorCourses().subscribe((courses) => {
+          this.courses = courses;
+        });
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
 }
