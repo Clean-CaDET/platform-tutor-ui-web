75ec2e5e0d2596bda7849197f67ad89d9523a2fd
Nikola Luburic <luburic91@gmail.com>
2024-07-05 13:56:25 +0200

    feat: Migrates all authoring controls to A15.

--- src/app/modules/learning-utilities/learning-utilities.module.ts
+++ src/app/modules/learning-utilities/learning-utilities.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MarkdownModule } from 'ngx-markdown';
 import { MaterialModule } from 'src/app/infrastructure/material.module';
@@ -13,7 +12,6 @@ import { TutorMarkdownModule } from 'src/app/shared/markdown/markdown.module';
     CommonModule,
     FormsModule,
     MaterialModule,
-    FlexLayoutModule,
     MarkdownModule.forChild(),
     ReactiveFormsModule,
     TutorMarkdownModule
--- src/app/modules/learning/learning.module.ts
+++ src/app/modules/learning/learning.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
