ca312c375cbcd9d670ae6f43943e3068e5cf8ae9
Nikola Luburic <luburic91@gmail.com>
2024-08-16 13:07:36 +0200

    feat: Learners can view the best before date on the unit overview page.

--- src/app/modules/learning/course/course-units/course-units.component.ts
+++ src/app/modules/learning/course/course-units/course-units.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Course } from '../../model/course.model';
@@ -9,9 +9,9 @@ import { CourseService } from '../course.service';
   templateUrl: './course-units.component.html',
   styleUrls: ['./course-units.component.scss'],
 })
-export class CourseUnitsComponent implements OnInit {
+export class CourseUnitsComponent implements OnChanges {
   @Input() course: Course;
-  masteredUnitIds: number[];
+  now: Date;
 
   constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, private courseService: CourseService) {
     iconRegistry.addSvgIcon(
@@ -20,17 +20,18 @@ export class CourseUnitsComponent implements OnInit {
     );
   }
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    this.now = new Date();
     const unitIds: number[] = [];
-    this.course.knowledgeUnits.forEach(
-      unit => unitIds.push(unit.id)
-    )
-    this.courseService.getMasteredUnitIds(unitIds)
-      .subscribe(masteredUnitIds => this.masteredUnitIds = masteredUnitIds);
+    this.course.knowledgeUnits.forEach(unit => {
+      unit.bestBefore = new Date(unit.bestBefore); // Transform ISO 8601 string to date for comparison.
+      if(unit.enrollmentStatus === "Completed") return;
+      unitIds.push(unit.id)
+    })
+    this.courseService.getMasteredUnitIds(this.course.id, unitIds)
+      .subscribe(masteredUnitIds => masteredUnitIds.forEach(id => {
+        const unit = this.course.knowledgeUnits.find(u => u.id === id);
+        unit.enrollmentStatus = "Completed";
+      }));
   }
-
-  isMastered(unitId: number): boolean {
-    return this.masteredUnitIds?.includes(unitId);
-  }
-
 }
