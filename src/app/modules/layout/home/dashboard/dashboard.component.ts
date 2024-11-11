96301a2c3ce010a06941f530733a1d6077a9eb8f
Nikola Luburic <luburic91@gmail.com>
2024-11-11 12:57:25 +0100

    fix: Makes the starting course and unit navigation more consistent with the rest of the Tutor.

--- src/app/modules/layout/home/dashboard/dashboard.component.ts
+++ src/app/modules/layout/home/dashboard/dashboard.component.ts
@@ -1,15 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { User } from 'src/app/infrastructure/auth/user.model';
 import { Course } from 'src/app/modules/learning/model/course.model';
+import { LayoutService } from '../../layout.service';
 
 @Component({
   selector: 'cc-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnChanges {
   @Input() user: User;
-  @Input() courses: Course[];
+  courses: Course[];
 
-  constructor() {}
+  constructor(private layoutService: LayoutService) {}
+
+  ngOnChanges(): void {
+    if (!this.user) {
+      this.courses = [];
+      return;
+    }
+    if (this.user.role.includes('learner')) {
+      this.layoutService.getLearnerCourses().subscribe((coursesPage) => {
+        this.courses = coursesPage.results.sort((a, b) => new Date(b.startDate).getTime() - new Date(a.startDate).getTime());
+      });
+    } else if (this.user.role === 'instructor') {
+      this.layoutService.getInstructorCourses().subscribe((courses) => {
+        this.courses = courses.sort((a, b) => new Date(b.startDate).getTime() - new Date(a.startDate).getTime());
+      });
+    }
+  }
 }
--- src/app/modules/learning/model/course.model.ts
+++ src/app/modules/learning/model/course.model.ts
@@ -4,6 +4,7 @@ export interface Course {
   id?: number;
   code: string;
   name: string;
+  isArchived?: boolean;
   description: string;
   startDate: Date;
   knowledgeUnits?: Unit[];
