a018ca98cafd98d73a959b18b50f0929f2579b60
Nikola Luburić <luburic91@gmail.com>
2026-03-09 07:09:30 +0100

    feat!: modernize Docker and CI/CD for Angular 21
    BREAKING CHANGE: requires Node.js 22+ (dropped Node 18 support)

--- docker/scripts/build-app.sh
+++ docker/scripts/build-app.sh
@@ -2,6 +2,6 @@ SMART_TUTOR_API_URL=$1
 
 export API_HOST=${SMART_TUTOR_API_URL}
 envsubst < ./docker/front/environment.ts.template > ./src/environments/environment.ts || exit
-npm run build --prod && \
+npm run build && \
 cd dist && \
 mv "$(find . -maxdepth 1 -type d | tail -n 1)" /app
